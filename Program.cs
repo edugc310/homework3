3e4da7d3634a3e5068a92b5a568e477fbae57b15
Eduardo J Garcia-Colon <eduardo.j.garcia-colon@boeing.com>
2022-06-07 11:45:06 -0700

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,8 @@
+﻿// See https://aka.ms/new-console-template for more information
+using homework3.Db;
+
+Console.WriteLine("Hello, World!" );
+ContactsContext contacts = new ContactsContext();
+contacts.Add( new Contact() );
+Console.WriteLine(contacts);
+contacts.SaveChanges();
